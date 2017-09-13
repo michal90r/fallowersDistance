13a8f69f812ecd1a977c2096cad5448c79e00681
Michal Ryzio <michal90r@gmail.com>
2017-09-13 15:12:21 +0200

    add stubbed react stateful component

---
+++ client/src/Container/UsersDistance.js
@@ -0,0 +1,83 @@
+import React from 'react';
+import {createStore} from 'redux'
+import {Provider, connect} from 'react-redux'
+
+import {Followers} from '../View/Followers'
+
+function reducer(state = [
+    {
+        username: "aigeano",
+        distance: 11963
+    },
+    {
+        username: "monica017",
+        distance: 9189
+    },
+    {
+        username: "fxrcode",
+        distance: 3609
+    },
+    {
+        username: "stuartpb",
+        distance: 193
+    },
+    {
+        username: "jb55",
+        distance: 2
+    },
+    {
+        username: "kbwatts",
+        distance: 2
+    }
+], action) {
+    switch (action.type) {
+        case 'SEARCH_USER': {
+            const searchUser = {
+                username: action.username,
+            };
+            return //:TODO
+        }
+        default: {
+            return state
+        }
+    }
+}
+
+function searchUser(username) {
+    return {
+        type: 'SEARCH_USER',
+        username: username,
+    };
+}
+
+
+
+const store = createStore(reducer);
+
+const mapStateToFollowersProps = (state) => {
+    return {
+        rows : state
+    };
+};
+
+const mapDispatchToFollowersProps = (dispatch) => (
+    {
+        onFormSubmit: (e) => (
+            dispatch(searchUser(e.username))
+        )
+    }
+);
+
+const UsersDistance = connect(
+    mapStateToFollowersProps,
+    mapDispatchToFollowersProps
+)(Followers);
+
+
+const WrappedUserDistance = () => (
+    <Provider store={store}>
+        <UsersDistance/>
+    </Provider>
+);
+
+export default WrappedUserDistance;
---
+++ client/src/View/Followers.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import Form from '../View/Form'
+import List from '../View/List'
+
+import '../App.css';
+
+
+const Followers = (props) => (
+    <div>
+        <h1>{props.title}</h1>
+        <div id="formWrapper">
+            <Form
+                onSubmit={props.onFormSubmit}
+            />
+        </div>
+        <div id="listWrapper">
+            <List
+                rows={props.rows}
+            />
+        </div>
+    </div>
+);
+
+export {Followers};

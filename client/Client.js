770b33f31e209c968677fb5caf42c8ea20adc68d
Michal Ryzio <michal90r@gmail.com>
2017-09-13 13:55:38 +0200

    add rest to Client

---
+++ client/Client.js
@@ -0,0 +1,34 @@
+import fetch from 'isomorphic-fetch';
+
+
+const Client = {
+    getTenTheFarthestFollowers(username) {
+        const url = 'api/distance?username?=' + username;
+        return fetch(url, {
+            method: 'get',
+            header: {
+                accept: 'application/json',
+            }
+        }).then(this.checkStatus)
+            .then(this.parseJson)
+    },
+
+    checkStatus(response) {
+        if (response.status >= 200 && response.status < 300) {
+            return response;
+        } else {
+            const error = new Error(`HTTP Error ${response.statusText}`);
+            error.status = response.statusText;
+            error.response = response;
+            console.log(error);
+            throw error;
+        }
+    },
+
+    parseJson(response) {
+        return response.json();
+    }
+
+};
+
+export default Client
--- dist/client/Client.js
+++
@@ -1,2 +0,0 @@
-"use strict";
-//# sourceMappingURL=Client.js.map
